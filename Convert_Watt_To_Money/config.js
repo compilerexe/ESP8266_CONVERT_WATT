7754895543dceea55fb1ca892139f4848cf4ffc7
compilerexe <compilerexe@gmail.com>
2015-07-03 22:35:15 +0700

    ESP8266 Convert Watt To Money

---
+++ Convert_Watt_To_Money/config.js
@@ -0,0 +1,11 @@
+// host = '172.16.153.122';	// hostname or IP address
+host = '128.199.104.122';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 9001;
+topic = 'esp8266/18:fe:34:a6:f6:16/status';		// topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+// username = "jjolie";
+// password = "aa";
+cleansession = true;
