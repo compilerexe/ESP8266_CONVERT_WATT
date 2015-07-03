9a7c510554fdda0299c5ad2ef064cf827f3a52ec
compilerexe <compilerexe@gmail.com>
2015-07-03 23:00:50 +0700

    no message

--- Convert_Watt_To_Money/config.js
+++ Convert_Watt_To_Money/config.js
@@ -1,5 +1,5 @@
 // host = '172.16.153.122';	// hostname or IP address
-host = '128.199.104.122';	// hostname or IP address
+host = '_Your_IP_MQTT_';	// hostname or IP address
 // host = '172.16.153.110';	// hostname or IP address
 port = 9001;
 topic = 'esp8266/18:fe:34:a6:f6:16/status';		// topic to subscribe to
